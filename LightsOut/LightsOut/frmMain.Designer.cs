c00f0f3c4dc27fb510d2893f939937c0510d4924
rnppnr <45872624+rnppnr@users.noreply.github.com>
2018-12-27 15:08:32 +0000

    Initial commit

---
+++ LightsOut/LightsOut/frmMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace LightsOut
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(519, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(519, 517);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ LightsOut/LightsOut/frmMain.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class frmMain : Form
+    {
+        private Random rnd = new Random();
+        private PictureBox[,] cellArray = new PictureBox[5, 5];
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    cellArray[row, col] = new PictureBox();
+                    cellArray[row, col].Tag = row + "," + col;
+                    cellArray[row, col].Width = 98;
+                    cellArray[row, col].Height = 98;
+                    cellArray[row, col].Left = (col * 100) + 10;
+                    cellArray[row, col].Top = (row * 100) + 10;
+                    cellArray[row, col].BackColor = Color.DarkGreen;
+                    cellArray[row, col].MouseClick += new MouseEventHandler(cellArray_Click);
+                    this.Controls.Add(cellArray[row, col]);
+                }
+            }
+
+            foreach (var item in this.Controls)
+            {
+                if (item is PictureBox)
+                {
+                    PictureBox pb = (PictureBox)item;
+                    pb.BackColor = (rnd.Next(0, 100) < 90) ? Color.DarkGreen : Color.Green;
+                }
+            }
+        }
+
+        private void cellArray_Click(object sender, MouseEventArgs e)
+        {
+            int row = 0,
+                col = 0;
+
+            foreach (var item in this.Controls)
+            {
+                if (item is PictureBox)
+                {
+                    PictureBox ca = (PictureBox)item;
+                    if (ca.Bounds.Contains(PointToClient(MousePosition)))
+                    {                        
+                        string[] cell = ca.Tag.ToString().Split(',');
+                        row = Int16.Parse(cell[0]);
+                        col = Int16.Parse(cell[1]);
+                        ca.BackColor = Color.Green;
+                        ToggleColours(row, col);
+                    }                    
+                }                
+            }
+        }
+
+        private void ToggleColours(int row, int col)
+        {
+            try
+            {
+                if (row > 0)
+                    cellArray[row - 1, col].BackColor = (cellArray[row, col].BackColor == Color.Green) ? Color.DarkGreen : Color.Green;
+                if (row < 4)
+                    cellArray[row + 1, col].BackColor = (cellArray[row, col].BackColor == Color.Green) ? Color.DarkGreen : Color.Green;
+                if (col > 0)
+                    cellArray[row, col - 1].BackColor = (cellArray[row, col].BackColor == Color.Green) ? Color.DarkGreen : Color.Green;
+                if (col < 4)
+                    cellArray[row, col + 1].BackColor = (cellArray[row, col].BackColor == Color.Green) ? Color.DarkGreen : Color.Green;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
